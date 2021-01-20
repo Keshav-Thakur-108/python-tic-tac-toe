8413135c7e6b262114144063ffd29c5b15567fc5
Keshav <keshavthakur.001@gmail.com>
2021-01-20 21:52:10 +0530

    Created player_choice and replay funcs

--- index.py
+++ index.py
@@ -62,3 +62,21 @@ def full_board_check(board):
         if space_check(board, i):
             return False
     return True
+
+
+def player_choice(board):
+    position = 0
+
+    while position not in [1, 2, 3, 4, 5, 6, 7, 8, 9] or not space_check(board, position):
+        position = int(input('Choose your next position: (1-9) '))
+
+    return position
+
+
+def replay():
+    choice = input("Do you want to play again? Enter 'YES' or 'NO': ")
+    if choice == 'NO':
+        return True
+    else:
+        return False
+    pass
