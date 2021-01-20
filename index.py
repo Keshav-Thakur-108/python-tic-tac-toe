a7a2c28c1d9429e21f23bd155b1d6e0fd7561ad0
Keshav <keshavthakur.001@gmail.com>
2021-01-20 21:47:11 +0530

    Created display_board and player_input funcs

---
+++ index.py
@@ -0,0 +1,31 @@
+import random
+from IPython.display import clear_output
+
+
+def display_board(board):
+    clear_output()  # Remember, this only works in jupyter!
+    print('   |   |')
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print('   |   |')
+
+
+def player_input():
+    marker = ''
+    while marker != 'X' and marker != 'O':
+        marker = input("Choose between X or O: ")
+        player1 = marker
+
+    if player1 == 'X':
+        player2 = 'O'
+    else:
+        player2 = 'X'
+
+    return (player1, player2)
