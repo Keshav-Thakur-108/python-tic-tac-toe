9113e43bbb40b63882c47efed84731879c84d1f8
Keshav <keshavthakur.001@gmail.com>
2021-01-20 21:51:13 +0530

    Created win_check, space_check, full_board_check, place_markerand choose_first funcs

--- index.py
+++ index.py
@@ -29,3 +29,36 @@ def player_input():
         player2 = 'X'
 
     return (player1, player2)
+
+
+def win_check(board, mark):
+    mark_list = [mark, mark, mark]
+    if (board[1:4] == mark_list or board[4:7] == mark_list or board[7:10] == mark_list or board[1:8:3] == mark_list or board[2:9:3] == mark_list or board[3:10:3] == mark_list or board[1:10:4] == mark_list or board[3:8:2] == mark_list):
+        return True
+    else:
+        return False
+
+
+def choose_first():
+    if random.randint(0, 1) == 0:
+        return 'Player 2'
+    else:
+        return 'Player 1'
+
+
+def place_marker(board, marker, position):
+    board[position] = marker
+
+
+def space_check(board, position):
+    if board[position] != 'X' and board[position] != 'O':
+        return True
+    else:
+        return False
+
+
+def full_board_check(board):
+    for i in range(1, 10):
+        if space_check(board, i):
+            return False
+    return True
